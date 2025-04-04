c48ab04e039265d2a038006b3de2c3a5b220830f
madhuk2015 <madhukolli2003@gmail.com>
2025-04-03 21:02:52 -0400

    Update deploy_notebooks.sql

---
+++ scripts/deploy_notebooks.sql
@@ -0,0 +1,29 @@
+--!jinja
+
+/*-----------------------------------------------------------------------------
+Hands-On Lab: Intro to Data Engineering with Notebooks
+Script:       deploy_notebooks.sql
+Author:       Jeremiah Hansen
+Last Updated: 6/11/2024
+-----------------------------------------------------------------------------*/
+
+-- See https://docs.snowflake.com/en/LIMITEDACCESS/execute-immediate-from-template
+
+-- Create the Notebooks
+--USE SCHEMA {{env}}_SCHEMA;
+use schema DEV_SCHEMA;
+
+CREATE OR REPLACE NOTEBOOK IDENTIFIER('"{{ env_var('SF_DATABASE') }}"."DEV_SCHEMA"."{{ env_var('SF_DATABASE') }}_notebook_1"')
+    FROM '@"DEMO_DB"."PUBLIC"."snowflake_extensions"/branches/"{{branch}}"/notebook_1/'
+    QUERY_WAREHOUSE = '{{ env_var('SF_WAREHOUSE') }}'
+    MAIN_FILE = 'sample_code.ipynb';
+
+ALTER NOTEBOOK "{{ env_var('SF_DATABASE') }}"."DEV_SCHEMA"."{{ env_var('SF_DATABASE') }}_notebook_1" ADD LIVE VERSION FROM LAST;
+/*------------------------------
+CREATE OR REPLACE NOTEBOOK IDENTIFIER('"DEMO_DB"."{{env}}_SCHEMA"."{{env}}_07_load_daily_city_metrics"')
+    FROM '@"DEMO_DB"."INTEGRATIONS"."DEMO_GIT_REPO"/branches/"{{branch}}"/notebooks/07_load_daily_city_metrics/'
+    QUERY_WAREHOUSE = 'DEMO_WH'
+    MAIN_FILE = '07_load_daily_city_metrics.ipynb';
+
+ALTER NOTEBOOK "DEMO_DB"."{{env}}_SCHEMA"."{{env}}_07_load_daily_city_metrics" ADD LIVE VERSION FROM LAST;
+-----------------------------------*/
