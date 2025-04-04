749001b039f6c9bf41af1bcc2854a7f82e24d1d0
madhuk2015 <madhukolli2003@gmail.com>
2025-04-04 08:19:12 -0400

    Update deploy_notebooks.sql

--- scripts/deploy_notebooks.sql
+++ scripts/deploy_notebooks.sql
@@ -10,7 +10,6 @@ Last Updated: 6/11/2024
 -- See https://docs.snowflake.com/en/LIMITEDACCESS/execute-immediate-from-template
 
 -- Create the Notebooks
---USE SCHEMA {{env}}_SCHEMA;
 use schema DEV_SCHEMA;
 
 CREATE OR REPLACE NOTEBOOK IDENTIFIER('"{{DB}}"."DEV_SCHEMA"."{{DB}}_notebook_1"')
@@ -19,11 +18,3 @@ CREATE OR REPLACE NOTEBOOK IDENTIFIER('"{{DB}}"."DEV_SCHEMA"."{{DB}}_notebook_1"
     MAIN_FILE = 'sample_code.ipynb';
 
 ALTER NOTEBOOK "{{DB}}"."DEV_SCHEMA"."{{DB}}_notebook_1" ADD LIVE VERSION FROM LAST;
-/*------------------------------
-CREATE OR REPLACE NOTEBOOK IDENTIFIER('"DEMO_DB"."{{env}}_SCHEMA"."{{env}}_07_load_daily_city_metrics"')
-    FROM '@"DEMO_DB"."INTEGRATIONS"."DEMO_GIT_REPO"/branches/"{{branch}}"/notebooks/07_load_daily_city_metrics/'
-    QUERY_WAREHOUSE = 'DEMO_WH'
-    MAIN_FILE = '07_load_daily_city_metrics.ipynb';
-
-ALTER NOTEBOOK "DEMO_DB"."{{env}}_SCHEMA"."{{env}}_07_load_daily_city_metrics" ADD LIVE VERSION FROM LAST;
------------------------------------*/
